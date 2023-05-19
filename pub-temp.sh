a7c7ded8d693a1524fd559f3b855a024e793624f
ahmed <alomairi@mail.ru>
2023-05-19 17:39:03 +0300

    add GPU temp

--- pub-temp.sh
+++ pub-temp.sh
@@ -1,21 +1,29 @@
 #!/usr/ash
 # using sh on lightweight alpine but you need to install
 # apk update && apk add jq mosquitto-clients
-rm ./temp.json
+
 while true
 do
+# directory for CPU temp
+FirstDir="/sys/class/hwmon/hwmon0/"
+# directory for GPU
+SecondDir="/sys/class/hwmon/hwmon2/"
 echo "{\"Server\":\"StrongServer\"" > temp.json
-for file in /sys/class/hwmon/hwmon0/*_input; do
+for file in $FirstDir*_input; do
         #echo "filename : ${file##*/}" ;
         ToNumber=${file##*/temp} ;
         onlyNumber=${ToNumber%%_*} ;
         #echo "ToNumber : $onlyNumber "
-        CatLabel=$(cat /sys/class/hwmon/hwmon0/"temp""$onlyNumber""_label")
-        CatInput=$(cat /sys/class/hwmon/hwmon0/"temp""$onlyNumber""_input")
+        CatLabel=$(cat $FirstDir"temp""$onlyNumber""_label")
+        CatInput=$(cat $FirstDir"temp""$onlyNumber""_input")
         echo "Temperture of $CatLabel is $((CatInput/1000))"
         echo "," >> temp.json
         echo "\"$CatLabel\":$((CatInput/1000))" >> temp.json
 done
+CatLabel2=$(cat $SecondDir"in0_label")
+CatInput2=$(cat $SecondDir"temp1_input")
+echo "," >> temp.json
+echo "\"$CatLabe2\":$CatInput2" >> temp.json
 echo "}" >> temp.json
 # Publish Data
 server=$(jq -r '.Server' ./secret.ini)
